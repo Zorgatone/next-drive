134a2cd26d2d21d878c11e9e3964b19eacbae0e6
Zorgatone <zorgatone@gmail.com>
2024-10-19 11:37:30 +0200

    Base template layout of page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,55 @@
 <script lang="ts">
+  import TopAppBar from "@smui/top-app-bar";
+  import Drawer, { AppContent, Content } from "@smui/drawer";
 </script>
 
-<main>
-  Hello
-</main>
+<div class="app">
+  <div class="top-bar" style="display: none"></div>
+  <TopAppBar variant="static" color="secondary" class="top-bar">Test</TopAppBar>
+  <div class="drawer-container">
+    <Drawer>
+      <Content class="drawer-content">Ciao</Content>
+    </Drawer>
+    <AppContent class="app-content">
+      <main class="main-content">Hello</main>
+    </AppContent>
+  </div>
+</div>
 
 <style>
+  .app {
+    width: 100%;
+    height: 100%;
+  }
+
+  .drawer-container {
+    position: relative;
+    display: flex;
+    height: calc(100% - 60px);
+    overflow: hidden;
+    z-index: 0;
+  }
+
+  .app :global(.top-bar) {
+    height: 60px;
+    padding: 16px;
+  }
+
+  .drawer-container :global(.drawer-content) {
+    padding: 16px;
+  }
+
+  .drawer-container :global(.app-content) {
+    flex: auto;
+    overflow: auto;
+    position: relative;
+    flex-grow: 1;
+  }
+
+  .main-content {
+    overflow: auto;
+    padding: 16px;
+    height: 100%;
+    box-sizing: border-box;
+  }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,8 @@
-import './style.css'
+import "./style.css"
+
+// import "svelte-material-ui/themes/svelte-dark.css"
+import "svelte-material-ui/themes/svelte.css"
+
 import App from './App.svelte'
 
 const app = new App({
