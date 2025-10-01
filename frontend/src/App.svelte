85a620f40a6783961cc460239176022f954e83c1
Tommaso Ricci <zorgatone@gmail.com>
2025-10-01 13:42:41 +0200

    Updated frontend dependencies, new Svelte version, removed UI lib

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,55 +1,15 @@
 <script lang="ts">
-  import TopAppBar from "@smui/top-app-bar";
-  import Drawer, { AppContent, Content } from "@smui/drawer";
+  // TODO: implementation
 </script>
 
-<div class="app">
-  <div class="top-bar" style="display: none"></div>
-  <TopAppBar variant="static" color="secondary" class="top-bar">Test</TopAppBar>
-  <div class="drawer-container">
-    <Drawer>
-      <Content class="drawer-content">Ciao</Content>
-    </Drawer>
-    <AppContent class="app-content">
-      <main class="main-content">Hello</main>
-    </AppContent>
-  </div>
+<div>
+  <!-- TODO: implementation -->
+  <h1>TODO: implementation</h1>
 </div>
 
 <style>
-  .app {
-    width: 100%;
-    height: 100%;
-  }
-
-  .drawer-container {
-    position: relative;
-    display: flex;
-    height: calc(100% - 60px);
-    overflow: hidden;
-    z-index: 0;
-  }
-
-  .app :global(.top-bar) {
-    height: 60px;
-    padding: 16px;
-  }
-
-  .drawer-container :global(.drawer-content) {
-    padding: 16px;
-  }
-
-  .drawer-container :global(.app-content) {
-    flex: auto;
-    overflow: auto;
-    position: relative;
-    flex-grow: 1;
-  }
-
-  .main-content {
-    overflow: auto;
-    padding: 16px;
-    height: 100%;
-    box-sizing: border-box;
+  /* TODO: implementation */
+  h1 {
+    margin: 0;
   }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,8 @@
 import "./style.css"
 
-// import "svelte-material-ui/themes/svelte-dark.css"
-import "svelte-material-ui/themes/svelte.css"
-
+import { mount } from 'svelte';
 import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app')
-})
+const app = mount(App, { target: document.getElementById("app")! });
 
-export default app
+export default app;
